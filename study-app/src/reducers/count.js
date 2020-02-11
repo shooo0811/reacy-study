29eb0fd75c4596da2ad1165f2eb155b40335821b
kuboshota <shooo0811@gmail.com>
2020-02-11 21:03:04 +0900

    counter app(+-)

--- study-app/src/reducers/count.js
+++ study-app/src/reducers/count.js
@@ -1,13 +1,15 @@
 import { INCREMENT, DECREMENT } from '../actions'
 
-const initialStae = { value: 0 }
+const initialState = { value: 0 }
 
-export default ( state = initialStae, action ) => {
+export default (state = initialState, action) => {
     switch(action.type){
         case INCREMENT:
             return { value: state.value + 1 }
         case DECREMENT:
             return { value: state.value - 1 }
+        default:
+            return state
 
     }
 }
