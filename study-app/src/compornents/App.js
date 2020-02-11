588550cca7f91b0c157e9b4249f68f5258e8fc6c
kuboshota <shooo0811@gmail.com>
2020-02-11 22:19:17 +0900

    evets app create

--- study-app/src/compornents/App.js
+++ study-app/src/compornents/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-import { increment, decrement } from '../actions'
+import { increment, decrement, multiplication } from '../actions'
 
 class App extends Component{
   render() {
@@ -12,6 +12,7 @@ class App extends Component{
         <div>value: { props.value }</div>
         <button onClick={props.increment}>+1</button>
         <button onClick={props.decrement}>-1</button>
+        <button onClick={props.multiplication}>*</button>
       </React.Fragment>
     )
   }
@@ -21,6 +22,7 @@ const mapStateToProps = state => ({ value: state.count.value })
 const mapDispatchToProps = dispatch => ({
   increment: () => dispatch(increment()),
   decrement: () => dispatch(decrement()),
+  multiplication: () => dispatch(multiplication()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
