facaa6ced20934918fa6360cffa95ae86f22f953
kuboshota <shooo0811@gmail.com>
2020-02-17 21:52:06 +0900

    event new

--- my-app/src/components/events_new.js
+++ my-app/src/components/events_new.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-//import { Field, reduxForm } from 'redux-form'
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Field, reduxForm } from 'redux-form'
 //import RaisedButton from 'material-ui/RaisedButton'
 //import TextField from 'material-ui/TextField'
 
 //import { postEvent } from '../actions'
 
 class EventsNew extends Component {
-  render() {
+    renderField(field){
+        const { input, label, type, meta: { tocthed, error } } = field
+        return(<div></div>)
+    }
+    render() {
       return(
-          <div>うんこ</div>
+        <form>
+            <div>
+                <Field label='Title' name='title' type='text' component={this.renderField} />
+            </div>
+            <div>
+                <Field label='Body' name='body' type='text' component={this.renderField} />
+            </div>
+            <div>
+                <input type='submit' value='Submit' disabled={false} />
+                <Link to='/'>Chancel</Link>
+            </div>
+        </form>
       )
-  }
+    }
 }
 
-//const mapDispatchToProps = { postEvent }
+const validation = values => {
+    const errors = {}
+    return errors
+}
 
-export default connect(null, null)(EventsNew)
+export default connect(null, null)(
+    reduxForm({ validation, form: 'eventNewForm' })(EventsNew)
+)
